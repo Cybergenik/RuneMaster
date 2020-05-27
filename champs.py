1601ee404f7c9a369eba22bb1b87cf194bcf513f
Luciano Remes <cybergenik@gmail.com>
2020-05-26 19:47:05 -0600

    fixing weird champ names

--- champs.py
+++ champs.py
@@ -8,7 +8,7 @@ class Champ:
         self.real = False
         for name in response.json()['data']:
             if champ == name.lower():
-                self.champ = response.json()['data'][name]['name']
+                self.champ = response.json()['data'][name]['id']
                 self.title = response.json()['data'][self.champ]['title']
                 self.img = 'https://ddragon.leagueoflegends.com/cdn/10.10.3216176/img/champion/'+response.json()['data'][self.champ]['image']['full']
                 self.desc =  response.json()['data'][self.champ]['blurb']
@@ -20,6 +20,12 @@ class Champ:
                             Attack Damage: {response.json()["data"][self.champ]["stats"]["attackdamage"]} \n \
                             Attack Range: {response.json()["data"][self.champ]["stats"]["attackrange"]} \n \
                             Attack Speed: {response.json()["data"][self.champ]["stats"]["attackspeed"]}'
+                if ' ' in response.json()['data'][self.champ]['name']:
+                    self.link = 'https://leagueoflegends.fandom.com/wiki/'+response.json()['data'][self.champ]['name'].replace(' ', '_')
+                elif '\'' in response.json()['data'][self.champ]['name']:
+                    self.link = 'https://leagueoflegends.fandom.com/wiki/'+response.json()['data'][self.champ]['name'].replace('\'','%27')
+                else:
+                    self.link = 'https://leagueoflegends.fandom.com/wiki/'+response.json()['data'][self.champ]['name']
                 self.real = True 
                 break
 
