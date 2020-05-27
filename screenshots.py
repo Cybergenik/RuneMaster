6846eb3568eb2aa91768de9a4b4599bcbfdae1b1
Luciano Remes <cybergenik@gmail.com>
2020-05-27 00:40:42 -0600

    wtf

--- screenshots.py
+++ screenshots.py
@@ -14,9 +14,10 @@ class Screenshots:
                 self.champ = response.json()['data'][name]['name']
                 URL = 'https://champion.gg/champion/'+self.champ
                 options = webdriver.FirefoxOptions()
-                options.headless = True
+                options.add_argument('--headless')
                 options.add_argument('--no-sandbox')
                 print(os.getenv("FIREFOX_BIN"))
+                print(os.getenv("GECKODRIVER_PATH"))
                 options.binary_location = os.getenv("FIREFOX_BIN")
                 self.driver = webdriver.Firefox(executable_path=os.getenv("GECKODRIVER_PATH"),options=options)
                 self.driver.get(URL)
