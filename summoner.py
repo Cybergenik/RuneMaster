052c34dc1b6e1a9ae68d7c7caf054a8d8f402dd2
Luciano Remes <cybergenik@gmail.com>
2020-05-29 02:13:30 -0600

    get_matches again

--- summoner.py
+++ summoner.py
@@ -80,7 +80,7 @@ class Summon:
 
     def get_matches(self):
         S = lambda X: self.driver.execute_script('return document.querySelector("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.RealContent > div").scroll'+X)
-        self.driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
+        self.driver.set_window_size(S('Width'),S('Height')+200) # May need manual adjustment
         seed = str(random.randint(0,99999))
         self.driver.find_element_by_xpath('//*[@id="SummonerLayoutContent"]/div[2]/div[2]/div/div[2]').screenshot('./images/vape'+seed+'.png')
         return seed
