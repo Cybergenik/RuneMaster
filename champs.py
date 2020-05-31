d431267102c1d9eed2e81a444eb0f7a22fe9f20f
Luciano Remes <cybergenik@gmail.com>
2020-05-31 00:03:43 -0600

    using different website

--- champs.py
+++ champs.py
@@ -20,7 +20,7 @@ class Champ():
                             Attack Damage: {response[self.champ]["stats"]["attackdamage"]} \n \
                             Attack Range: {response[self.champ]["stats"]["attackrange"]} \n \
                             Attack Speed: {response[self.champ]["stats"]["attackspeed"]}'
-                self.url = 'https://champion.gg/champion/'+self.champ
+                self.url = f'https://www.op.gg/champion/{self.champ}/statistics'
                 self.real = True 
                 self.name = response[name]['name']
                 if driver != None:
@@ -29,35 +29,35 @@ class Champ():
                 break
 
     def runes(self):
-        self.driver.set_window_size(689,1120) # May need manual adjustment        
+        #self.driver.set_window_size(689,1120) # May need manual adjustment        
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('//*[@id="perks-app"]/div').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[1]/div/div[1]/div/table/tbody[2]').screenshot('./temp/'+seed+'.png')
         return seed
 
     def build(self):
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[2]/div[2]/div/div/div[2]/div[1]/div[1]').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[1]/div/div[1]/table[2]').screenshot('./temp/'+seed+'.png')
         return seed
 
     def skills(self):
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[2]/div[2]/div/div/div[1]/div[1]').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[1]/div/div[1]/table[1]/tbody[2]').screenshot('./temp/'+seed+'.png')
         return seed
 
     def champ_stats(self):
-        self.driver.set_window_size(1080,1920)
+        #self.driver.set_window_size(1080,1920)
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[2]/div[1]/div/div/div[2]').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[1]/div/div[2]/div[1]').screenshot('./temp/'+seed+'.png')
         return seed
         
     def sums(self):
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[2]/div[1]/div/div/div[4]/div[7]').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[1]/div/div[1]/table[1]/thead[1]').screenshot('./temp/'+seed+'.png')
         return seed
 
     def matchups(self):
-        S = lambda X: self.driver.execute_script('return document.querySelector("body > div > div.main-container > div.page-content > div.ng-scope > div.matchups > div > div.row.counter-row").scroll'+X)
-        self.driver.set_window_size(S('Width')+200,S('Height')+200)
+        #S = lambda X: self.driver.execute_script('return document.querySelector("body > div > div.main-container > div.page-content > div.ng-scope > div.matchups > div > div.row.counter-row").scroll'+X)
+        #self.driver.set_window_size(S('Width')+200,S('Height')+200)
         seed = str(random.randint(0,99999))
-        self.driver.find_element_by_xpath('/html/body/div/div[2]/div[3]/div[2]/div[3]/div/div[2]').screenshot('./temp/'+seed+'.png')
+        self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div[5]/div[7]/div/div[2]/div[3]').screenshot('./temp/'+seed+'.png')
         return seed
