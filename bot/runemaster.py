b8a1a39fdedee578c17e8614cd529424b9ba988a
Luciano <cybergenik@gmail.com>
2021-07-29 23:39:31 -0600

    test

--- bot/runemaster.py
+++ bot/runemaster.py
@@ -8,6 +8,7 @@ from bot.utils import TOKEN, VERSION, COMMANDS, REGIONS, TIERS, real_champ, real
 
 client = Client()
 
+#TODO: test something
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
