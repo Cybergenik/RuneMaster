6d9bb5f305d1d7a6bf860d5641da3ec1fd5b5594
Luciano <cybergenik@gmail.com>
2022-01-17 21:48:13 -0700

    adding different sh scripts for prod and dev

---
+++ prod_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install libnspr4 libatk1.0-0 libatk-bridge2.0-0 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2 libatspi2.0-0 libwebkit2gtk-4.0-dev\
+&& export PYTHONPATH=$PYTHONPATH:./bot \
+&& pip install -r requirements.txt \
+&& python3.9 -m playwright install chromium \
+&& python3.9 bot/runemaster.py &
